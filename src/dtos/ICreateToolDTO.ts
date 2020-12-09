9da56115e30fb1ba7bc8689a53e93f8a456f9704
Gustavo <gbs.shadow@gmail.com>
2020-12-09 19:09:05 -0300

    Fix interface

--- src/dtos/ICreateToolDTO.ts
+++ src/dtos/ICreateToolDTO.ts
@@ -1,10 +1,13 @@
-import Tag from '../models/Tag';
 import User from '../models/User';
+
+interface ITags {
+  tag_id: string;
+}
 
 export default interface ICreateToolDTO {
   user: User;
   title: string;
   link: string;
   description: string;
-  tags: Tag[];
+  tags: ITags[];
 }
