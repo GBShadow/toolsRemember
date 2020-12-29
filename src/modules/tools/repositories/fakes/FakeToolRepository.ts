03f68f716bab222018b42a24af8d211ac402b9a2
Gustavo <gbs.shadow@gmail.com>
2020-12-29 16:43:57 -0300

    Starr create Fake Repository

---
+++ src/modules/tools/repositories/fakes/FakeToolRepository.ts
@@ -0,0 +1,86 @@
+import { uuid } from 'uuidv4';
+import IToolsRepository from '@modules/tools/repositories/IToolsRepository';
+import IFindByIdDTO from '@modules/tools/dtos/IFindByIdDTO';
+import IFindByTitleDTO from '@modules/tools/dtos/IFindByTitleDTO';
+import IFindByTagDTO from '@modules/tools/dtos/IFindByTagDTO';
+import ICreateToolDTO from '@modules/tools/dtos/ICreateToolDTO';
+import Tool from '../../infra/typeorm/entities/Tool';
+
+class FakeToolsRepository implements IToolsRepository {
+  private tools: Tool[] = [];
+
+  public async create({
+    user,
+    title,
+    link,
+    description,
+    tags,
+  }: ICreateToolDTO): Promise<Tool> {
+    const tool = new Tool();
+
+    Object.assign(tool, {
+      id: uuid,
+      user,
+      title,
+      link,
+      description,
+      tools_tags: tags,
+    });
+
+    this.tools.push(tool);
+
+    return tool;
+  }
+
+  public async findAll(user_id: string): Promise<Tool[]> {
+    const findAllToolsOfUser = this.tools.filter(
+      tool => tool.user_id === user_id,
+    );
+
+    return findAllToolsOfUser;
+  }
+
+  public async findById({
+    user_id,
+    id,
+  }: IFindByIdDTO): Promise<Tool | undefined> {
+    const findTool = this.tools.find(tool => {
+      return tool.user_id === user_id && tool.id === id;
+    });
+
+    return findTool;
+  }
+
+  public async findByTitle({
+    user_id,
+    title,
+  }: IFindByTitleDTO): Promise<Tool | undefined> {
+    const findTool = this.tools.find(tool => {
+      return tool.user_id === user_id && tool.title === title;
+    });
+
+    return findTool;
+  }
+
+  public async findAllByTag({
+    user_id,
+    tagId,
+  }: IFindByTagDTO): Promise<Tool[]> {
+    const findToolsByTag = this.tools.filter(tool => {
+      return (
+        tool.user_id === user_id &&
+        tool.tools_tags.find(tool_tag => tool_tag.tag_id === tagId)
+      );
+    });
+
+    return findToolsByTag;
+  }
+
+  public async deleteTool(tool: Tool): Promise<void> {
+    const indexTool = this.tools.findIndex(t => t === tool);
+
+    this.tools.slice(1, indexTool);
+  }
+}
+
+export default FakeToolsRepository;
