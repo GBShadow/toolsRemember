1260bae73d1d003ca4f7ce58d9c933bde7122ed8
GBShadow <gbs.shadow@gmail.com>
2021-01-09 13:40:17 -0300

    Tests list tools

---
+++ src/modules/tools/services/ListToolsByTagService.spec.ts
@@ -0,0 +1,155 @@
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUserRepository';
+import AppError from '@shared/errors/AppError';
+import FakeTagsRepository from '../repositories/fakes/FakeTagRepository';
+import FakeToolRepository from '../repositories/fakes/FakeToolRepository';
+import ListToolsByTagService from './ListToolsByTagService';
+import CreateToolService from './CreateToolService';
+
+let fakeToolRepository: FakeToolRepository;
+let fakeUserRepository: FakeUsersRepository;
+let fakeTagRepository: FakeTagsRepository;
+let createTool: CreateToolService;
+let listToolsByTag: ListToolsByTagService;
+
+describe('ListToolsByTagService', () => {
+  beforeEach(() => {
+    fakeToolRepository = new FakeToolRepository();
+    fakeUserRepository = new FakeUsersRepository();
+    fakeTagRepository = new FakeTagsRepository();
+
+    listToolsByTag = new ListToolsByTagService(
+      fakeToolRepository,
+      fakeUserRepository,
+      fakeTagRepository,
+    );
+
+    createTool = new CreateToolService(
+      fakeToolRepository,
+      fakeUserRepository,
+      fakeTagRepository,
+    );
+  });
+
+  it('should be able to list tools with the same tag', async () => {
+    const user = await fakeUserRepository.create({
+      name: 'John Doe',
+      email: 'john@john.com',
+      password: '123456',
+    });
+
+    await createTool.execute({
+      user_id: user.id,
+      title: 'Visual Studio Code',
+      link: 'vscode.com.br',
+      description: 'A text editor',
+      tags: ['Gui', 'Free', 'Text editor'],
+    });
+
+    const tool1 = await createTool.execute({
+      user_id: user.id,
+      title: 'Neovim',
+      link: 'neovim.com.br',
+      description: 'A simple text editor',
+      tags: ['Terminal', 'Free', 'Text editor'],
+    });
+
+    const tool2 = await createTool.execute({
+      user_id: user.id,
+      title: 'Vim',
+      link: 'vim.com.br',
+      description: 'A simple text editor',
+      tags: ['Terminal', 'Free', 'Text editor'],
+    });
+
+    expect(
+      await listToolsByTag.execute({
+        user_id: user.id,
+        tag: 'Terminal',
+      }),
+    ).toMatchObject([tool1, tool2]);
+  });
+
+  it('should be able to list all tools without using a tag', async () => {
+    const user = await fakeUserRepository.create({
+      name: 'John Doe',
+      email: 'john@john.com',
+      password: '123456',
+    });
+
+    const tool1 = await createTool.execute({
+      user_id: user.id,
+      title: 'Neovim',
+      link: 'neovim.com.br',
+      description: 'A simple text editor',
+      tags: ['Terminal', 'Free', 'Text editor'],
+    });
+
+    const tool2 = await createTool.execute({
+      user_id: user.id,
+      title: 'Vim',
+      link: 'vim.com.br',
+      description: 'A simple text editor',
+      tags: ['Terminal', 'Free', 'Text editor'],
+    });
+
+    const tool3 = await createTool.execute({
+      user_id: user.id,
+      title: 'Visual Studio Code',
+      link: 'vscode.com.br',
+      description: 'A text editor',
+      tags: ['Gui', 'Free', 'Text editor'],
+    });
+
+    expect(
+      await listToolsByTag.execute({
+        user_id: user.id,
+      }),
+    ).toMatchObject([tool1, tool2, tool3]);
+  });
+
+  it('should not be able to list tools with a invalid user', async () => {
+    const user = await fakeUserRepository.create({
+      name: 'John Doe',
+      email: 'john@john.com',
+      password: '123456',
+    });
+
+    await createTool.execute({
+      user_id: user.id,
+      title: 'Neovim',
+      link: 'neovim.com.br',
+      description: 'A simple text editor',
+      tags: ['Terminal', 'Free', 'Text editor'],
+    });
+
+    await expect(
+      listToolsByTag.execute({
+        user_id: 'invalid_user_id',
+        tag: 'Terminal',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able a list tool if does not exist', async () => {
+    const user = await fakeUserRepository.create({
+      name: 'John Doe',
+      email: 'john@john.com',
+      password: '123456',
+    });
+
+    await createTool.execute({
+      user_id: user.id,
+      title: 'Neovim',
+      link: 'neovim.com.br',
+      description: 'A simple text editor',
+      tags: ['Terminal', 'Free', 'Text editor'],
+    });
+
+    await expect(
+      listToolsByTag.execute({
+        user_id: user.id,
+        tag: 'invalid_tag',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
--- src/modules/tools/services/ListToolsByTagService.ts
+++ src/modules/tools/services/ListToolsByTagService.ts
@@ -1,4 +1,5 @@
 import AppError from '@shared/errors/AppError';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import Tool from '../infra/typeorm/entities/Tool';
 
 import ITagsRepository from '../repositories/ITagsRepository';
@@ -6,24 +7,35 @@ import IToolsRepository from '../repositories/IToolsRepository';
 
 interface IRequest {
   user_id: string;
-  tag: string | null;
+  tag?: string;
 }
 
-class FindToolService {
+class ListToolsByTagService {
   private toolRepository;
+
+  private userRepository;
 
   private tagRepository;
 
   constructor(
     toolRepository: IToolsRepository,
+    userRepository: IUsersRepository,
     tagRepository: ITagsRepository,
   ) {
     this.toolRepository = toolRepository;
+
+    this.userRepository = userRepository;
 
     this.tagRepository = tagRepository;
   }
 
   public async execute({ user_id, tag }: IRequest): Promise<Tool[]> {
+    const user = await this.userRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('User does not exist');
+    }
+
     if (!tag) {
       const tools = await this.toolRepository.findAll(user_id);
 
@@ -44,4 +56,4 @@ class FindToolService {
   }
 }
 
-export default FindToolService;
+export default ListToolsByTagService;
